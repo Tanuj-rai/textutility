e691b5bb0da6849765ab5a229e0aceaef00d958b
Tanuj Rai <tanujrai898@gmail.com>
2022-09-01 17:24:25 +0530

    my react project

--- src/App.js
+++ src/App.js
@@ -1,25 +1,60 @@
-import logo from './logo.svg';
-import './App.css';
+// import { useState } from "react";
+import "./App.css";
+import Navbar from "./components/Navbar";
+import TextForm from "./components/Textform";
+import About from "./components/About";
+import React, { useState } from 'react';
+import Alert from "./components/Alert";
+
+import {
+  BrowserRouter,
+  Routes, 
+  Route
+} from "react-router-dom";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  
+    const [mode, setMode] = useState('light');
+    const [alert, setAlert] = useState(null);
+    
+    const showAlert = (message, type) =>{
+      setAlert({
+        msg:message,
+        type:type
+      })
+      setTimeout(() =>{
+        setAlert(null);
+      }, 1500);
+
+    }
+    const toggleMode=()=>{
+    if (mode==='light'){
+        setMode('dark');
+        document.body.style.background = '#042743';
+        showAlert('Dark mode has been enabled',"success");
+        
+  }
+    else {
+        setMode('light');
+        document.body.style.background = 'white';
+        showAlert('Light mode has been enabled',"success");
+  }
 }
+  return (
+    <>
+    <BrowserRouter>
+    <Navbar title="TextUtility" mode = {mode} toggleMode={toggleMode} />
+    <Alert alert = {alert}/>
+    <div className="container my-3">
+      <Routes>
+      <Route exact path="/about" element={<About/>} />
+      <Route path="/" element={<TextForm heading = "Enter your text to analyse" mode ={mode}/>} />
+       
+      </Routes>
+    </div>
+    </BrowserRouter>
+    </>
+  );
+  }
 
 export default App;
